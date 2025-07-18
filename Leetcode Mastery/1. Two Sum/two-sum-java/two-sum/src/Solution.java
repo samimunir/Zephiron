98b4716bfd09ce5d54aba82a2c595f15dec27314
Sami Munir <samidmunir@outlook.com>
2025-07-17 22:14:47 -0400

    implementing java solution(3) for leetcode #1 - two sum

--- Leetcode Mastery/1. Two Sum/two-sum-java/two-sum/src/Solution.java
+++ Leetcode Mastery/1. Two Sum/two-sum-java/two-sum/src/Solution.java
@@ -68,4 +68,25 @@ public class Solution {
         }
         return null;
     }
+
+    /*
+     * Algorithm:
+     * 
+     * Runtime: 2ms
+     * Memory: 45.24 MB
+     */
+    public int[] solutionThree(int[] nums, int target) {
+        HashMap<Integer, Integer> numsMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!numsMap.containsKey(nums[i])) {
+                numsMap.put(nums[i], i);
+            }
+            int complement = target - nums[i];
+            if (numsMap.containsKey(complement) && numsMap.get(complement) != i) {
+                return new int[] {i, numsMap.get(complement)};
+            }
+        }
+        return null;
+    }
 }
