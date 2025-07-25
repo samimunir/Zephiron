5a439c5a1ce0afa603737d54c07ca7f48244b24e
Sami Munir <samidmunir@outlook.com>
2025-07-24 21:03:41 -0400

    implementing product-controller & route deleteProduct()

--- Projects/e-commerce-store/backend/controllers/product.controller.js
+++ Projects/e-commerce-store/backend/controllers/product.controller.js
@@ -68,3 +68,32 @@ export const createProduct = async (req, res) => {
       .json({ message: "Server error.", error: error.message });
   }
 };
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found." });
+    }
+
+    if (product.image) {
+      const publicId = product.image.split("/").pop().split(".")[0];
+
+      try {
+        await cloundinary.uploader.destroy(`products/${publicId}`);
+        console.log("-> Deleted image from cloudinary.");
+      } catch (error) {
+        console.log("Error deleting image from cloudinary:", error);
+      }
+    }
+
+    await Product.findByIdAndDelete(req.params.id);
+
+    return res.json({ message: "Product deleted successfully." });
+  } catch (error) {
+    console.log("Error in deleteProduct() controller:", error.message);
+    return res
+      .status(500)
+      .json({ message: "Server error.", error: error.message });
+  }
+};
