e1d5c3dabe438f3056a324056d73c2f7bc9a6463
Sami Munir <samidmunir@outlook.com>
2025-07-24 20:47:08 -0400

    implementing product-controller + route getFeaturedProducts()

--- Projects/e-commerce-store/backend/controllers/product.controller.js
+++ Projects/e-commerce-store/backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -7,6 +8,29 @@ export const getAllProducts = async (req, res) => {
     return res.json({ products });
   } catch (error) {
     console.log("Error in getAllProducts() controller:", error.message);
+    return res
+      .status(500)
+      .json({ message: "Server error.", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found." });
+    }
+
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    return res.json({ featuredProducts });
+  } catch (error) {
+    console.log("Error in getFeaturedProducts() controller:", error.message);
     return res
       .status(500)
       .json({ message: "Server error.", error: error.message });
