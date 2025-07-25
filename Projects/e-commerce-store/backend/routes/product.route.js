ba9cdb2234fdf1ad93208110ede7dbb8ccb3b29c
Sami Munir <samidmunir@outlook.com>
2025-07-24 20:30:15 -0400

    implementing protectRoute() middleware function

--- Projects/e-commerce-store/backend/routes/product.route.js
+++ Projects/e-commerce-store/backend/routes/product.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getAllProducts } from "../controllers/product.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
