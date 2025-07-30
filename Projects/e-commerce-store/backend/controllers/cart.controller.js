d8b675c7fc87fce3ca537629ca1cf5c36f07fa32
Sami Munir <samidmunir@outlook.com>
2025-07-30 15:54:36 -0400

    completing Cart controller > getCartProducts() + updateQuantity()

--- Projects/e-commerce-store/backend/controllers/cart.controller.js
+++ Projects/e-commerce-store/backend/controllers/cart.controller.js
@@ -1,4 +1,24 @@
-export const getCartProducts = async (req, res) => {};
+import Product from "../models/product.model.js";
+
+export const getCartProducts = async (req, res) => {
+  try {
+    const products = await Product.find({ _id: { $in: req.user.cartItems } });
+
+    const cartItems = products.map((product) => {
+      const item = req.user.cartItems.find(
+        (cartItem) => cartItem.id === product.id
+      );
+      return { ...product.toJSON(), quantity: item.quantity };
+    });
+
+    return res.json(cartItems);
+  } catch (error) {
+    console.log("Error in getCartProducts() controller:", error.message);
+    return res
+      .status(500)
+      .json({ message: "Server error.", error: error.message });
+  }
+};
 
 export const addToCart = async (req, res) => {
   try {
@@ -45,4 +65,34 @@ export const removeAllFromCart = async (req, res) => {
   }
 };
 
-export const updateQuantity = async (req, res) => {};
+export const updateQuantity = async (req, res) => {
+  try {
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+    if (existingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+
+        await user.save();
+
+        return res.json(user.cartItems);
+      }
+
+      existingItem.quantity = quantity;
+
+      await user.save();
+
+      return res.json(user.cartItems);
+    } else {
+      return res.status(404).json({ message: "Product not found." });
+    }
+  } catch (error) {
+    console.log("Error in updateQuantity() controller:", error.message);
+    return res
+      .status(500)
+      .json({ message: "Server error.", error: error.message });
+  }
+};
