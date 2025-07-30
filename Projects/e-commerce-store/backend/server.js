43895a4c5436623449578cc6f3e454e21344ad01
Sami Munir <samidmunir@outlook.com>
2025-07-30 15:59:57 -0400

    creating Coupon model Schema

--- Projects/e-commerce-store/backend/server.js
+++ Projects/e-commerce-store/backend/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
