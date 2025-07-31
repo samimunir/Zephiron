10c73237b812777c68c618d0e191a9c429878a51
Sami Munir <samidmunir@outlook.com>
2025-07-30 23:32:35 -0400

    completing Analytics router & controller

--- Projects/e-commerce-store/backend/server.js
+++ Projects/e-commerce-store/backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
